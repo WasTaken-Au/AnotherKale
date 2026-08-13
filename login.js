2c8de4ddb1a10179619454e4a0818c46ddd316b9
au <nakayamaeiyu@gmail.com>
2026-08-14 03:52:27 +0900

    change link(again)

--- login.js
+++ login.js
@@ -8,7 +8,7 @@ function validateUserInput() {
     if (emailList.includes(inputName)) {
         const emailTag = emailList.indexOf(inputName)
         if (inputPassword === passwordList[emailTag]) {
-            window.location.href = "src/pages/home.html"
+            window.location.href = "WasTaken-Au/AnotherKale/src/pages/home.html"
             alert("Login successful.")
         } else {
             alert("Password is incorrect.")
