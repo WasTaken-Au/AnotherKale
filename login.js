a5583094180c229372d280a02d8ee11f38c19c95
au <nakayamaeiyu@gmail.com>
2026-08-14 03:25:57 +0900

    Add files via upload

---
+++ login.js
@@ -0,0 +1,19 @@
+const emailList = ["fake@email.com"]
+const passwordList = ["111"]
+
+function validateUserInput() {
+    const inputName = document.getElementById('usernameInput').value
+    const inputPassword = document.getElementById('passwordInput').value
+
+    if (emailList.includes(inputName)) {
+        const emailTag = emailList.indexOf(inputName)
+        if (inputPassword === passwordList[emailTag]) {
+            window.location.href = "../Kale/src/pages/home.html"
+            alert("Login successful.")
+        } else {
+            alert("Password is incorrect.")
+        }
+    } else {
+        alert("Email does not exist.")
+    }
+}
